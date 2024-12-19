55bf11803cedff59402ef8d1ffb55227d78b745c
Aryaman Sharma <200314arya@gmail.com>
2024-12-19 13:55:54 +0530

    final

--- script.js
+++ script.js
@@ -1,69 +1,56 @@
 window.onload = fetchData;
-document.getElementById("searchBtn").addEventListener("click", fetchDataSrch);
-
+document.getElementById("searchBtn").addEventListener("click", searchpoke);
 function fetchData() {
-  const apiURL = "https://pokeapi.co/api/v2/pokemon/?limit=50&offset=50";
-  fetch(apiURL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=50&offset=50";
+  fetch(url)
+    .then((res) => {
+    return res.json();
     })
-    .then((data) => {
-      const dataList = document.getElementById("dataList");
-      dataList.innerHTML = "";
-      data.results.forEach((item) => {
+    .then((value) => {
+      const List = document.getElementById("dataList");
+      value.results.forEach((item) => {
         const li = document.createElement("li");
-
         const anchor = document.createElement("a");
         anchor.textContent = item.name;
         anchor.href = "#";
-        li.addEventListener("click", () => fetchPokemonDetails(item.name));
-
+        li.addEventListener("click", 
+        () => fetchPokemonDetails(item.name));
         li.appendChild(anchor);
-        dataList.appendChild(li);
+        List.appendChild(li);
       });
     })
-    .catch((error) => {
-      console.error("Error", error);
+    .catch(() => {
+      alert("Error in fetching 50 pokemon");
     });
 }
 
-function fetchPokemonDetails(pokemonName) {
-  const apiURL = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-  fetch(apiURL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Pokémon not found! status: ${response.status}`);
-      }
-      return response.json();
+function fetchPokemonDetails(pokename) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokename}`;
+  fetch(url)
+    .then((res) => {
+    return res.json();
     })
     .then((data) => {
-      const clickedPokemonDetails = document.getElementById("clicked-pokemon-details");
-      clickedPokemonDetails.innerHTML = ` 
-        <h2>${data.name}</h2>
-        <strong>ID:</strong> ${data.id} <br>
-        <strong>Height:</strong> ${data.height} <br>
-        <strong>Weight:</strong> ${data.weight} <br>
-        <strong>Types:</strong> ${data.types.map((type) => type.type.name).join(", ")} <br>
-        <strong></strong> <img src="${data.sprites.front_default}" alt="${data.name}" />
+      const pokeClick = document.getElementById("clicked-pokemon-details");
+      pokeClick.innerHTML = ` 
+        <h2>${data.name}</h2>  
+        <b>height:</b> ${data.height} <br>
+        <b>weight:</b> ${data.weight} <br>
+        <img src="${data.sprites.front_default}" alt="${data.name}" />
       `;
     })
-    .catch((error) => {
-      console.error("Error fetching Pokémon details:", error);
-      alert("Error fetching Pokémon details.");
+    .catch(() => {
+      alert("Error fetching pokemon.");
     });
 }
 
-function fetchDataSrch() {
-  const searchInput = document.getElementById("searchInput").value.trim().toLowerCase();
-  if (searchInput === "") {
-    alert("Please enter a Pokémon name to search.");
+function searchpoke() {
+  const input = document.getElementById("input").value.trim().toLowerCase();
+  if (input === "") {
+    alert("Enter pokemon name.");
     return;
   }
-
-  fetchPokemonDetails(searchInput);
+  else {
+  fetchPokemonDetails(input);
+  }
 }
-    
-
